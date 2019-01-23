9eee45ad98f9bded8dba1fd59acfc1c3276c22c2
TylerJNewman <tylerjnewman18@gmail.com>
2019-01-23 14:55:23 -0800

    add title back

--- src/App.js
+++ src/App.js
@@ -19,7 +19,7 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <Header />
+        <Header title="Todo List" />
         <Content>
           <TodoList />
         </Content>
