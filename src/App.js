2ed025da90e04e059c7139b86fd881da5e9a24c5
TylerJNewman <tylerjnewman18@gmail.com>
2019-01-23 13:11:30 -0800

    add card to TodoListItem

--- src/App.js
+++ src/App.js
@@ -10,36 +10,12 @@ import TodoList from "./components/todos/TodoListContainer";
 
 window.allTodos = allTodos;
 
-const Card = () => {
-  return (
-    <div className="row">
-      <div className="col s12 m6 offset-m3">
-        <div className="card blue-grey darken-1">
-          <div className="card-content white-text">
-            <span className="card-title">Card Title</span>
-            <p>
-              I am a very simple card. I am good at containing small bits of
-              information. I am convenient because I require little markup to
-              use effectively.
-            </p>
-          </div>
-          <div className="card-action">
-            <a href="#">This is a link</a>
-            <a href="#">This is a link</a>
-          </div>
-        </div>
-      </div>
-    </div>
-  );
-};
-
 class App extends Component {
   render() {
     return (
       <div className="App">
         <Header />
         <Content>
-          <Card />
           <TodoList />
         </Content>
         <Footer />
--- src/components/todos/TodoListItem.js
+++ src/components/todos/TodoListItem.js
@@ -3,7 +3,22 @@ import React from "react";
 class TodoListItem extends React.Component {
   render() {
     const { todo, updateTodo } = this.props;
-    return <li>{todo.title}</li>;
+    return (
+      <div className="row">
+        <div className="col s12 m6 offset-m3">
+          <div className="card blue-grey darken-1">
+            <div className="card-content white-text">
+              <span className="card-title">{todo.title}</span>
+              <p>{todo.body}</p>
+            </div>
+            <div className="card-action">
+              <a href="#">This is a link</a>
+              <a href="#">This is a link</a>
+            </div>
+          </div>
+        </div>
+      </div>
+    );
   }
 }
 
