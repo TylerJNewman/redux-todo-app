5ffe050640f7f40e0ea99319ae91e47e26a64aa1
TylerJNewman <tylerjnewman18@gmail.com>
2019-01-24 13:56:06 -0800

    try scale in animation

--- src/components/todos/TodoListItem.js
+++ src/components/todos/TodoListItem.js
@@ -1,24 +1,38 @@
 import React from "react";
 
 class TodoListItem extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = { scaleIn: "scale-transition scale-out" };
+  }
+
+  componentDidMount() {
+    setTimeout(() => {
+      this.setState({ scaleIn: "scale-transition scale-in" });
+    }, 1);
+  }
   render() {
     const { todo, updateTodo } = this.props;
+
     return (
-      <article className="row">
-        <div className="col s12 m6 offset-m3">
-          <div className="card blue-grey darken-1">
-            <div className="card-content white-text">
-              <span className="card-title">{todo.title}</span>
-              <p>{todo.body}</p>
-            </div>
-            <div className="card-action">
-              <a href="#!" className="delete">
-                <i className="material-icons">close</i>
-              </a>
+      <div class={this.state.scaleIn}>
+        <article className="row">
+          <div className="col s12 m6 offset-m3">
+            <div className="card blue-grey darken-1">
+              <div className="card-content white-text">
+                <span className="card-title">{todo.title}</span>
+                <p>{todo.body}</p>
+              </div>
+              <div className="card-action">
+                <a href="#!" className="delete">
+                  <i className="material-icons">close</i>
+                </a>
+              </div>
             </div>
           </div>
-        </div>
-      </article>
+        </article>
+      </div>
     );
   }
 }
