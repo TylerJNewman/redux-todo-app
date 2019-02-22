13845466b729ef0a18424bf230a6c63f17126db3
TylerJNewman <tylerjnewman18@gmail.com>
2019-02-22 10:05:24 -0800

    begin after css play

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,6 +3,41 @@ import React, { Component } from "react";
 class Header extends Component {
   // Here you can define your PropTypes.
 
+  // $(document).ready(function() {
+  //   var trig = 1;
+  //   //fix for chrome
+  //   $("#search").addClass('searchbarfix');
+
+  //   //animate searchbar width increase to  +150%
+  //   $('#search').click(function (e) {
+  //     //handle other nav elements visibility here to avoid push down
+  //     $('.search-hide').addClass('hide');
+  //     if (trig == 1) {
+  //       $('#navfix2').animate({
+  //         width: '+=150',
+  //         marginRight: 0
+  //       }, 400);
+
+  //       trig++;
+  //     }
+
+  //   });
+
+  //   // if user leaves the form the width will go back to original state
+
+  //   $("#search").focusout(function () {
+
+  //     $('#navfix2').animate({
+  //       width: '-=150'
+  //     }, 400);
+  //     trig = trig - 1;
+  //     //handle other nav elements visibility first to avoid push down
+  //     $('.search-hide').removeClass('hide');
+
+  //   });
+
+  // });
+
   render() {
     const { title } = this.props;
 
@@ -20,7 +55,7 @@ class Header extends Component {
                     <div id="navfix2">
                       <div className="input-field">
                         <input id="search" type="search" required />
-                        <label className="label-icon" for="search">
+                        <label className="label-icon" htmlFor="search">
                           <i className="material-icons">search</i>
                         </label>
                         <i className="material-icons">close</i>
